c43ac5865a75288338470ed269ee0d0d972676db
Om Kumar <om2005kumar001@gmail.com>
2025-02-05 02:01:40 +0530

    Firebase Setup

--- costomer-support/src/Firebase.js
+++ costomer-support/src/Firebase.js
@@ -1,7 +1,8 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import 'firebase/auth';
+import 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,4 +20,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+
+export const auth = firebaseApp.auth();
+export const firestore = firebaseApp.firestore();
+export default firebaseApp;
